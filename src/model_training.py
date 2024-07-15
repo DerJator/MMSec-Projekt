fd8d71940c56b8d0a6f759a4994c264e20589c77
Jonathan Stahlberger <jonathan.stahlberger@gmail.com>
2024-07-15 09:50:15 +0200

    Add validation

--- src/model_training.py
+++ src/model_training.py
@@ -1,17 +1,22 @@
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 import Casia2
 import models
 import first_compressions as cmp
 import argparse
 import wandb
+import random
 
-def train_model(model, train_loader, criterion, optimizer, num_epochs=10, device=torch.device("cpu"), log_interval=1):
+def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device=torch.device("cpu"), log_interval=1):
     for epoch in range(num_epochs):
+        train_loss = 0.0
+        val_loss = 0.0
+
+        """ TRAINING LOOP """
         model.train()
-        running_loss = 0.0
+
         for img1, img2, label in train_loader:
             # print(img1[0].size(), img1[1].size(), label)
             img1, img2, label = img1.to(device), img2.to(device), label.to(device)
@@ -24,12 +29,24 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs=10, device
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
+            train_loss += loss.item()
 
-        epoch_loss = running_loss / len(train_loader)
+        """ VALIDATION LOOP """
+        model.eval()
+        with torch.no_grad():
+            for img1, img2, label in val_loader:
+                img1, img2, label = img1.to(device), img2.to(device), label.to(device)
+                output1, output2 = model(img1, img2)
+                loss = criterion(output1, output2, label)
+
+                val_loss += loss.item()
+
+        epoch_train_loss = train_loss / len(train_loader.dataset)
+        epoch_val_loss = val_loss / len(val_loader.dataset)
+
         if epoch % log_interval == 0:
-            run.log({"loss": epoch_loss})  # Max 1x pro epoch (bei mehreren Sachen z.B. dict füllen)
-        print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+            run.log({"train-loss": epoch_train_loss, "val-loss": epoch_val_loss})  # Max 1x pro epoch (bei mehreren Sachen z.B. dict füllen)
+        print(f'Epoch {epoch + 1}/{num_epochs} \t| Train Loss: {epoch_train_loss:.4f} \t| Val Loss: {epoch_val_loss:.4f}')
 
 
 def evaluate_model(model, test_loader):
@@ -67,36 +84,49 @@ if __name__ == '__main__':
     DEVICE = torch.device("cuda")
     # DEVICE = torch.device("cpu")
 
-
-    # Initialize Dataset
+    """ Initialize Dataset """
     casia_data = Casia2.Casia2Dataset(DATA_PATH, channels=[i for i in range(N_CHANNELS)])
     casia_data.organize_output_pairs(mode="neg_pos")  # Also add positive pairs
     # print(casia_data.output_pairs)
-    train_pairs, test_pairs, train_labels, test_labels = casia_data.train_test_split(test=0.2)
+    train_pairs, test_pairs, train_labels, test_labels = casia_data.train_test_split(test=0.15)
 
+    train_size = int(0.8235 * len(train_pairs))  # 0.8235 * 75% is 70% of the original data
+    val_size = len(train_pairs) - train_size  # Remaining 15% of the original data
+
+    all_indices = list(range(len(train_pairs)))
+    random.shuffle(all_indices)
+    train_ix = all_indices[:train_size]
+    val_ix = all_indices[train_size:]
+
+    val_pairs = [train_pairs[i] for i in val_ix]
+    train_pairs = [train_pairs[i] for i in train_ix]
+    val_labels = [train_labels[i] for i in val_ix]
+    train_labels = [train_labels[i] for i in train_ix]
+    print(f"TRAIN SIZE: {len(train_pairs)}, VAL SIZE: {len(val_pairs)}, TEST SIZE: {len(test_pairs)}")
+
+    """ Transforms """
     transform = transforms.Compose([
         transforms.ToTensor(),
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    print(f"{len(train_pairs)=}")
-
-    """ Put images through AI Coder """
-    compressor = cmp.Compressor(cmp.CModelName.CHENG2020, device=DEVICE)
-
+    """ Package in Dataset and DataLoader """
     train_data = models.ImagePairsDataset(train_pairs, train_labels, transform)
+    val_data = models.ImagePairsDataset(val_pairs, val_labels, transform)
     test_data = models.ImagePairsDataset(test_pairs, test_labels, transform)
 
-    # print(train_data.image_pairs[0])
-
     train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=16, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=16, shuffle=False)
 
     # for i1, i2, label in train_loader:
         # print(type(i1), type(i2), type(label))
         # break
 
+    """ Put images through AI Coder """
+    compressor = cmp.Compressor(cmp.CModelName.CHENG2020, device=DEVICE)
     train_cmp_loader = compressor.compress_torch(train_loader)
+    val_cmp_loader = compressor.compress_torch(val_loader)
     test_cmp_loader = compressor.compress_torch(test_loader)
 
     """ FIRST PART: Train all Au-Tp pairs and the same number of Au-Au / Tp-Tp pairs """
@@ -115,7 +145,7 @@ if __name__ == '__main__':
 
     """ TRAINING """
 
-    train_model(model, train_cmp_loader, loss_fun, optimizer, device=DEVICE, num_epochs=50)
+    train_model(model, train_cmp_loader, val_cmp_loader, loss_fun, optimizer, device=DEVICE, num_epochs=50)
 
     # Evaluate the model
     evaluate_model(model, test_cmp_loader)
