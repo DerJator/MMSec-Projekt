Adapt pipeline, s.t. training of ESupConLoss Model is possible, albeit with possibly wrong error calculation

src/first_compressions.py
@@ -61,29 +61,30 @@ class Compressor:
         """
         Takes image and returns its latent representation and quantized latent representation
         """
-        if type(img) is np.ndarray:
-            img = img_to_torch(img)
-        elif type(img) is torch.Tensor and len(img.size()) == 3:
-            img = img.unsqueeze(0)
+        with torch.no_grad():
+            if type(img) is np.ndarray:
+                img = img_to_torch(img)
+            elif type(img) is torch.Tensor and len(img.size()) == 3:
+                img = img.unsqueeze(0)
 
-        h, w = img.shape[-2:]
-        pad, unpad = compute_padding(in_h=h, in_w=w, min_div=2 ** 6)
+            h, w = img.shape[-2:]
+            pad, unpad = compute_padding(in_h=h, in_w=w, min_div=2 ** 6)
 
-        padded_img = F.pad(input=img, pad=pad, mode="constant", value=0)
+            padded_img = F.pad(input=img, pad=pad, mode="constant", value=0)
 
-        padded_img = padded_img.to(device)
-        model = model.to(device)
+            padded_img = padded_img.to(device)
+            model = model.to(device)
 
-        y = model.g_a(padded_img)  # latent space direkt nach Encoder
-        y_hat = model.gaussian_conditional.quantize(y, mode="dequantize")  # latent space nach Quantisierung
+            y = model.g_a(padded_img)  # latent space direkt nach Encoder
+            y_hat = model.gaussian_conditional.quantize(y, mode="dequantize")  # latent space nach Quantisierung
 
-        if detach:
-            y_hat = y_hat.squeeze(0).detach().cpu().numpy().copy()
-        else:
-            y = y.squeeze(0)
-            y_hat = y_hat.squeeze(0)
+            if detach:
+                y_hat = y_hat.squeeze(0).detach().cpu().numpy().copy()
+            else:
+                y = y.squeeze(0)
+                y_hat = y_hat.squeeze(0)
 
-        return y, y_hat
+            return y, y_hat
 
 
 def cheng2020_model(quality: int = 6):

src/model_training.py
@@ -64,11 +64,11 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
 
         if epoch_val_loss < best_val_loss:
             best_val_loss = epoch_val_loss
+            print(f"Saving model at epoch {epoch + 1}")
             torch.save(model.state_dict(), f'../models/model_{epoch}.pth')
 
 
 def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device=torch.device("cpu"), log_interval=1):
-
 
     for epoch in range(num_epochs):
         train_loss = 0.0
@@ -90,15 +90,20 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
             if isinstance(criterion, models.ContrastiveLoss):
                 diff_label = torch.Tensor([(0 if label1[i] == label2[i] else 1) for i in range(len(label1))]).to(device)
                 loss = criterion(z_au.to(device), z_tp.to(device), diff_label)
+                print("Contrastive")
+                print(loss)
             elif isinstance(criterion, models.ESupConLoss):
                 prototypes = model.get_weights_class_layer()
                 loss = criterion(z_au.to(device), z_tp.to(device), prototypes.cuda())
+                del z_au, y_au, z_tp, y_tp, img1, img2, label1, label2, prototypes
+                print("ESupCon")
+                print(loss)
             else:
                 raise ValueError('Unsupported criterion')
             loss.backward()
             optimizer.step()
 
-            train_loss += loss.item()
+            train_loss += float(loss)
 
         """ VALIDATION LOOP """
         model.eval()
@@ -118,7 +123,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
                 else:
                     raise ValueError('Unsupported criterion')
 
-                val_loss += loss.item()
+                val_loss += float(loss)
 
         epoch_train_loss = train_loss / len(train_loader.dataset)
         epoch_val_loss = val_loss / len(val_loader.dataset)
@@ -147,24 +152,33 @@ def evaluate_model(model, test_loader):
     # print(f'Test Loss: {avg_loss:.4f}')
 
 
-def get_representations(model, data_loaders):
-    # Collect representations of training data (including validation data)
+def get_representations(model, data_loaders, as_torch=True):
+    # Collect representations of data (including validation data)
     representations = []
     labels = []
 
     for data_loader in data_loaders:
         for img1, img2, labels in data_loader:
-            print(labels)
+            print(f"{labels=}")
             label1, label2 = labels[0].cpu(), labels[1].cpu()
-            print(label1, label2)
-            representations.append(model.forward_representation(img1).cpu().detach().numpy())
-            representations.append(model.forward_representation(img2).cpu().detach().numpy())
+            print(f"{label1=}, {label2=}")
+            if as_torch:
+                representations.append(model.forward_representation(img1).cpu().detach())
+                representations.append(model.forward_representation(img2).cpu().detach())
+            else:
+                representations.append(model.forward_representation(img1).cpu().detach().numpy())
+                representations.append(model.forward_representation(img2).cpu().detach().numpy())
+
             labels.extend([label1, label2])
 
-    representations = np.concatenate(representations, axis=0)
+    # representations = np.concatenate(representations, axis=0)
     labels = np.concatenate(labels, axis=0)
 
     return representations, labels
+
+
+def print_memory_summary():
+    print(torch.cuda.memory_summary())
 
 
 if __name__ == '__main__':
@@ -227,9 +241,10 @@ if __name__ == '__main__':
     val_data = models.ImagePairsDataset(val_pairs, val_labels, transform)
     test_data = models.ImagePairsDataset(test_pairs, test_labels, transform)
 
-    train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+    val_loader = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+    test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False, pin_memory=True)
+
 
     """ Put images through AI Coder """
     compressor = cmp.Compressor(cmp.CModelName.CHENG2020, device=DEVICE)
@@ -265,6 +280,8 @@ if __name__ == '__main__':
     """ EVALUATION """
     torch.cuda.empty_cache()
     test_representations, test_labels = get_representations(trained_model, [test_cmp_loader])
+    # print(test_representations)
+    # print(type(test_representations))
 
     if MODEL_VERSION == 1:
         trained_model.eval()
@@ -287,11 +304,19 @@ if __name__ == '__main__':
     elif MODEL_VERSION == 2:
         trained_model.eval()
 
+        test_labels = []
         predictions = []
-        for img in test_representations:
-            predictions.append(trained_model.classify_sample(img))
+        for img1, img2, labels in test_cmp_loader:
+            for img in [img1, img2]:
+                probs = trained_model.classify_sample(img.to(DEVICE)).cpu()
+                label = torch.argmax(probs, dim=1)
+                predictions.extend(label.tolist())
+            test_labels.extend(labels[0].cpu().tolist())
+            test_labels.extend(labels[1].cpu().tolist())
 
         # Calculate the accuracy
+        print(predictions)
+        print(test_labels)
         accuracy = accuracy_score(test_labels, predictions)
         print(f"ESupCon Accuracy: {accuracy * 100:.2f}%")
         wandb.log({"ESupCon Accuracy": accuracy})

src/models.py
@@ -19,6 +19,7 @@ class SiameseNetwork(nn.Module):
         self.class_layer = None
         if class_layer:
             self.class_layer = nn.Linear(64, 2)
+            self.softmax = nn.Softmax(dim=1)
 
     def forward_representation(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -32,11 +33,11 @@ class SiameseNetwork(nn.Module):
         return z
 
     def forward_classifier(self, z):
-        return self.class_layer(z)
+        return self.softmax(self.class_layer(z))
 
     def forward(self, input1, input2):
         y1, y2 = None, None
-
+        print(f"input1: {input1.size()}")
         z1 = self.forward_representation(input1)
         if self.class_layer:
             y1 = self.forward_classifier(z1)
@@ -82,7 +83,7 @@ class ESupConLoss(nn.Module):
         # labels: Tensor of shape (batch_size,)
         self.n = z_au.size(0)
         self.n_k = self.n  # In each sample pair, there is 1 of each class
-        print(fc_weights.size())
+        print(f"{fc_weights.size()}")
         pt = fc_weights[0], fc_weights[1]  # TODO: Normalization
 
         supcon_loss = 0
@@ -95,9 +96,8 @@ class ESupConLoss(nn.Module):
                         (self.pt_loss(z_au, z_tp, pt)
                          + supcon_loss))
 
-        print(esupcon_loss)
-        print(esupcon_loss.size())  # TODO: Needs to be scalar, is a Tensor atm
-        return esupcon_loss
+        # print(f"{esupcon_loss.size()=}")  # TODO: Needs to be scalar, is a Tensor atm
+        return torch.sum(esupcon_loss)
 
     def pt_loss(self, z_au: torch.Tensor, z_tp: torch.Tensor, pts: tuple):
         """